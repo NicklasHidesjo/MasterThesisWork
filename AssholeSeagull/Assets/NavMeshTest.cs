285bc0c6528086a80c726e5c26eaf12c00360fbf
Emilie <emivog0223@skola.goteborg.se>
2021-11-16 19:45:27 +0100

    navmesh cont & work on crab

--- AssholeSeagull/Assets/NavMeshTest.cs
+++ AssholeSeagull/Assets/NavMeshTest.cs
@@ -6,34 +6,63 @@ using UnityEngine.AI;
 public class NavMeshTest : MonoBehaviour
 {
 	[SerializeField] private List<Transform> endPoint;
+    [SerializeField] private float maxDistance;
+
 	private int index = 0;
 	private NavMeshAgent agent;
 
-	void Start()
+	Animator animator;	
+
+    void Start()
 	{
 		agent = GetComponent<NavMeshAgent>();
+		animator = GetComponentInChildren<Animator>();
 		SetDestination();
 		IncreaseIndex();
 	}
 
 	private void Update()
 	{
-		Debug.Log("destination: " + agent.destination);
-		Debug.Log("position: " + transform.position);
+		animator.SetFloat("Speed", agent.speed);
 
-        if (!agent.pathPending)
-        {
-            if (agent.remainingDistance <= agent.stoppingDistance)
-            {
+		if (!agent.pathPending)
+		{
+			if (agent.remainingDistance <= agent.stoppingDistance)
+			{
 				IncreaseIndex();
 				SetDestination();
-            }
-        }
-    }
+			}
+		}
+	}
 
 	private void SetDestination()
 	{
-		agent.destination = endPoint[index].position;
+		Vector3 newDestination = GetRandomPoint(transform.position, maxDistance);
+		//agent.destination = endPoint[index].position;
+
+        if (newDestination == Vector3.zero)
+        {
+			SetDestination();
+        }
+        else
+        {
+			agent.destination = newDestination;
+        }
+	}
+
+	public static Vector3 GetRandomPoint(Vector3 center, float maxDistance)
+	{
+		// Get Random Point inside Sphere which position is center, radius is maxDistance
+		Vector3 randomPos = Random.insideUnitSphere * maxDistance + center;
+
+		NavMeshHit hit; // NavMesh Sampling Info Container
+
+        // from randomPos find a nearest point on NavMesh surface in range of maxDistance
+        if (NavMesh.SamplePosition(randomPos, out hit, maxDistance, NavMesh.AllAreas))
+        {
+		return hit.position;
+        }
+		return Vector3.zero;
 	}
 
 	private void IncreaseIndex()
