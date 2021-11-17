32e1e97521de56236f12b9b18c7c61fe39b5b127
Emilie <emivog0223@skola.goteborg.se>
2021-11-17 11:21:47 +0100

    NavMeshObstacle & scene work

--- AssholeSeagull/Assets/Scripts/Managers/GameSettings.cs
+++ AssholeSeagull/Assets/Scripts/Managers/GameSettings.cs
@@ -7,10 +7,10 @@ public class GameSettings : ScriptableObject
 {
     public GameModes GameMode;
 
-    public float GameDuration = 60f;
-    public bool TimerOff = true;
+    public float GameDuration = 120f;
+    public bool TimerOff = false;
     public bool AlwaysFreshFood = false;
-    public bool SeagullsDontAttack = true;
+    public bool SeagullsDontAttack = false;
     public int SeagullAmount = 5;
     public Vector2 SeagullSpawnInterval = new Vector2(3f,7f);
 }
